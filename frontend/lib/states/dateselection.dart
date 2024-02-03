1964796c5ec41593dc2c1be0d003957e3b6a2a9d
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2024-02-03 10:37:40 -0500

    Fix regex

--- frontend/lib/states/dateselection.dart
+++ frontend/lib/states/dateselection.dart
@@ -28,7 +28,7 @@ class _DateSelectionScreenState extends State<DateSelectionScreen>
   String selectedDay = "";
   String selectedMonth = "";
   String selectedYear = "";
-  RegExp regex = RegExp(r'^(0[1-9]|[1-2][0-9]|3[0-1])/(0?[1-9]|1[0-2])/\d{4}$');
+  RegExp regex = RegExp(r'^(0?[1-9]|[12][0-9]|3[01])/(0?[1-9]|1[0-2])/\d{4}$');
   @override
   String? get restorationId => widget.restorationId;
 
